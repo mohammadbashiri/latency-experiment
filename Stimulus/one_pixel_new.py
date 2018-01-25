546c1c201d630e2590f6d90310ed444870baae1b
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2018-01-25 14:27:37 +0100

    created stimulus class

---
+++ Stimulus/one_pixel_new.py
@@ -0,0 +1,41 @@
+import pyglet
+import itertools
+from stimulus import Stimulus
+import numpy as np
+
+platform = pyglet.window.get_platform()
+display = platform.get_default_display()
+screen = display.get_screens()[1]
+mywin = pyglet.window.Window(fullscreen=True, screen=screen)
+fps_display = pyglet.window.FPSDisplay(mywin)
+
+# initialize a stim object
+mypoint = Stimulus(window=mywin, type='POINT', x=mywin.width//2, y=mywin.height//2, width=4, height=4)
+
+colors = (
+    [255, 255, 255],
+    [255, 0, 0],
+    [0, 255, 0],
+    [0, 0, 255],
+    [255, 255, 0],
+    [255, 0, 255],
+    [0, 255, 255],
+)
+
+@mywin.event
+def on_draw():
+    mywin.clear()
+    for x, c in zip(np.linspace(950, 1050, len(colors)), colors):
+        mypoint.x = int(x)
+        mypoint.color = c
+        mypoint.draw()
+
+    fps_display.draw()
+
+heights = itertools.cycle([mywin.height//2, mywin.height//2 + 100])
+def update(dt, heights):
+    mypoint.y = next(heights)
+
+pyglet.clock.schedule(update, heights)
+
+pyglet.app.run()
--- Stimulus/stimulus.py
+++ Stimulus/stimulus.py
@@ -1,68 +1,107 @@
-from pyglet.gl import *
+import pyglet
+import itertools
+import numpy as np
 
-# Direct OpenGL commands to this window.
-platform = pyglet.window.get_platform()
-display = platform.get_default_display()
-screen = display.get_screens()[1]
-window = pyglet.window.Window(resizable=True)
+class plane:
 
-print(window.height, window.width)
+    def __init__(self, window=None, x=0, y=0, width=1, height=1, color=(255, 0, 0)):
 
-fac = 75
-size_h = 20
-size_w = 20
+        self._x = x
+        self._y = y
+        self._width = width
+        self._height = height
+        self._color = color * 4
+
+        self.vertices = pyglet.graphics.vertex_list(4,
+                                                    ('v2i', (self._x - self._width//2, self._y - self._height//2,
+                                                             self._x - self._width//2, self._y + self._height//2,
+                                                             self._x + self._width//2, self._y + self._height//2,
+                                                             self._x + self._width//2, self._y - self._height//2)),
+                                                    ('c3B', self._color)
+                                                    )
+
+class Stimulus:
+
+    def __init__(self, window=None, type='POINT', x=0, y=0, width=1, height=1, color=(255, 255, 255)):
+
+        self.type = type
+        self._win = window
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, val):
+        self._x = val
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, val):
+        self._y = val
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, val):
+        self._width = val
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, val):
+        self._height = val
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, val):
+        self._color = val
+
+    def draw(self):
+        if self.type == 'POINT':
+            self._obj = plane(window=self._win, x=self.x, y=self.y,
+                              width=self.width, height=self.height, color=self.color)
+            self._obj.vertices.draw(pyglet.gl.GL_POLYGON)
 
 
-i = 0
+
+if __name__ == '__main__':
+
+    platform = pyglet.window.get_platform()
+    display = platform.get_default_display()
+    screen = display.get_screens()[1]
+    mywin = pyglet.window.Window(fullscreen=True, screen=screen)
+
+    # initialize a stim object
+    mypoint = Stimulus(window=mywin, type='POINT', x=mywin.width//2, y=mywin.height//2, width=10, height=10)
 
 
-# Create the update function, for pyglet to run!
-def update(dt):
+    @mywin.event
+    def on_draw():
+        mywin.clear()
+        mypoint.draw()
 
-    global red_vertex, blue_vertex, i
+    heights = itertools.cycle([100,10])
+    colors = itertools.cycle([(255, 0, 0),(0, 255, 0)])
+    def update(dt):
+        Stimulus.x = next(heights)
+        Stimulus.color = next(colors)
 
-    if i%2 == 0:
-        red_vertex = pyglet.graphics.vertex_list(4,
-                                                 ('v2i', (window.width//2 - size_w//2, window.height//2 - size_h//2,
-                                                          window.width//2 - size_w//2, window.height//2 + size_h//2,
-                                                          window.width//2 + size_w//2, window.height//2 + size_h//2,
-                                                          window.width//2 + size_w//2, window.height//2 - size_h//2)),
-                                                 ('c3B', (255, 0, 0, 255, 0, 0, 255, 0, 0, 255, 0, 0))
-                                                 )
+    pyglet.clock.schedule_interval(update, 1)
 
-        blue_vertex = pyglet.graphics.vertex_list(4,
-                                                  ('v2i', (window.width//2 - size_w//2 - 50, window.height//2 - size_h//2,
-                                                           window.width//2 - size_w//2 - 50, window.height//2 + size_h//2,
-                                                           window.width//2 + size_w//2 - 50, window.height//2 + size_h//2,
-                                                           window.width//2 + size_w//2 - 50, window.height//2 - size_h//2)),
-                                                  ('c3B', (255, 0, 0, 255, 0, 0, 255, 0, 0, 255, 0, 0))
-                                                  )
-
-    else:
-        red_vertex = pyglet.graphics.vertex_list(4,
-                                                 ('v2i', (window.width//2 - size_w//2, window.height//2 - size_h//2,
-                                                          window.width//2 - size_w//2, window.height//2 + size_h//2,
-                                                          window.width//2 + size_w//2, window.height//2 + size_h//2,
-                                                          window.width//2 + size_w//2, window.height//2 - size_h//2)),
-                                                 ('c3B', (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
-                                                 )
-        blue_vertex = pyglet.graphics.vertex_list(4,
-                                                  ('v2i', (window.width//2 - size_w//2 - 50, window.height//2 - size_h//2,
-                                                           window.width//2 - size_w//2 - 50, window.height//2 + size_h//2,
-                                                           window.width//2 + size_w//2 - 50, window.height//2 + size_h//2,
-                                                           window.width//2 + size_w//2 - 50, window.height//2 - size_h//2)),
-                                                  ('c3B', (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
-                                                  )
-    i += 1
-
-pyglet.clock.schedule_interval(update, 1)
-
-update(0)
-
-@window.event
-def on_draw():
-
-    red_vertex.draw(pyglet.gl.GL_POLYGON)
-    blue_vertex.draw(pyglet.gl.GL_POLYGON)
-
-pyglet.app.run()
+    pyglet.app.run()
