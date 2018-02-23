3c018411c3c8d27a28492392613db89bb8a08705
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2018-02-23 18:40:20 +0100

    refactored script to separate function and script behavior.

--- Text_Files/plot_latency_hist.py
+++ Text_Files/plot_latency_hist.py
@@ -2,27 +2,41 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# get the file name
-path = input("Enter the file name:")
 
-# Import data
-data = pd.read_csv(path)
+def plot_latency_hist(df, win_size=9, threshold=5, ax=None):
+    """
+    Makes a latency plot from a dataframe made from the arduino_serial_read.py experiment.
 
-# Smoothing
-win_size = 9
-data[['Chan1_Smooth', 'Chan2_Smooth']] = data[['Chan1', 'Chan2']].rolling(win_size).max()
+    Arguments:
+        -win_size: the window size used for smoothing data.  Units are samples.
+        -threshold: the channel threshold used for frame detection.
 
-# Thresholding
-threshold = 5
-data[['Frame1On', 'Frame2On']] = data[['Chan1_Smooth', 'Chan2_Smooth']] > threshold
+    Returns:
+        Matplotlib axis.
+    """
 
-# Trial time detection
-data['TrialTime'] = data.groupby('Trial').Time.apply(lambda x: x - x.min())
+    data = df.copy()
 
-# Onset Detection for Each Trial
-resp_on = data[(data['Frame1On'] & (data['LED_State'] == 1)) | (data['Frame2On'] & (data['LED_State'] == 0))]
-latency = resp_on.groupby('Trial').TrialTime.min()
+    # Smooth Data and Detect Frames
+    data[['Chan1_Smooth', 'Chan2_Smooth']] = data[['Chan1', 'Chan2']].rolling(win_size).max()  # Smoothing
+    data[['Frame1On', 'Frame2On']] = data[['Chan1_Smooth', 'Chan2_Smooth']] > threshold  # Thresholding
 
-fig, ax = plt.subplots()
-(latency / 1000).hist(ax=ax).set(xlabel='latency time (ms)', ylabel='frequency')
-fig.savefig('s02_230218_white_randFreq_ObjectMode.png')
+    # Calculate Latency from LED Onset for each trial
+    data['TrialTime'] = data.groupby('Trial').Time.apply(lambda x: x - x.min())  # Trial time detection
+    resp_on = data[(data['Frame1On'] & (data['LED_State'] == 1)) | (data['Frame2On'] & (data['LED_State'] == 0))]
+    latency = resp_on.groupby('Trial').TrialTime.min()
+
+    # Plot the data
+    if not ax:
+        fig, ax = plt.subplots()
+    (latency / 1000).hist(ax=ax).set(xlabel='latency time (ms)', ylabel='frequency')
+    return ax
+
+
+if __name__ == '__main__':
+
+    # Import data
+    data = pd.read_csv('../Measurements/s02_230218_white_randFreq_ObjectMode.csv')
+    fig, ax = plt.subplots()
+    plot_latency_hist(df=data, win_size=9, threshold=5, ax=ax)
+    fig.savefig('s02_230218_white_randFreq_ObjectMode.png')
